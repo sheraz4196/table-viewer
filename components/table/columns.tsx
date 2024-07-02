614ff21f34bbc57c04584121fbb40133dea47301
sheraz4196 <sheraztoor321@gmail.com>
2024-07-02 10:53:27 +0500

    feat: column styling

--- components/table/columns.tsx
+++ components/table/columns.tsx
@@ -32,34 +32,107 @@ export const columns: ColumnDef<TableValues>[] = [
   },
   {
     accessorKey: "msrp",
-    header: "MSRP",
+    header: () => <div className="text-right">MSRP</div>,
+    cell: ({ row }) => {
+      const msrp = parseFloat(row.getValue("msrp"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(msrp);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
+
   {
     accessorKey: "leaveCredit",
-    header: "Leave Credit",
+    header: () => <div className="text-right">Leave Credit</div>,
+    cell: ({ row }) => {
+      const leaveCredit = parseFloat(row.getValue("leaveCredit"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(leaveCredit);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "residual",
-    header: "Residual",
+    header: () => <div className="text-right">Residual</div>,
+    cell: ({ row }) => {
+      const residual = parseFloat(row.getValue("residual"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(residual);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "moneyFactor",
-    header: "Money Factor",
+    header: () => <div className="text-right">Money Factor</div>,
+    cell: ({ row }) => {
+      const moneyFactor = parseFloat(row.getValue("moneyFactor"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(moneyFactor);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
-    accessorKey: "totalDriveOffa",
-    header: "Total Drive Offs",
+    accessorKey: "totalDriveOffs",
+    header: () => <div className="text-right">Total Drive Offs</div>,
+    cell: ({ row }) => {
+      const totalDriveOffs = parseFloat(row.getValue("totalDriveOffs"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(totalDriveOffs);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "terms",
-    header: "Terms",
+    header: () => <div className="text-right">Terms</div>,
+    cell: ({ row }) => {
+      const terms = parseFloat(row.getValue("terms"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(terms);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
-    accessorKey: "no-msds",
-    header: "No MSDs",
+    accessorKey: "noMSDs",
+    header: () => <div className="text-right">No MSDs</div>,
+    cell: ({ row }) => {
+      const noMSDs = parseFloat(row.getValue("noMSDs"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(noMSDs);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
   {
     accessorKey: "msds",
-    header: "MSDs",
+    header: () => <div className="text-right">MSDs</div>,
+    cell: ({ row }) => {
+      const msds = parseFloat(row.getValue("msds"));
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(msds);
+
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
   },
 ];
