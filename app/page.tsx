52485e41cde087d3f72353a7bd17c40a139e9e64
sheraz4196 <sheraztoor321@gmail.com>
2024-07-03 10:26:15 +0500

    fix: move data tab;e to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import SheetsTable from "@/components/general/sheets-table";
+import { columns } from "@/components/table/columns";
+import DataTable from "@/components/table/data-table";
 import { getMonthlyData } from "@/lib/new-data";
+import { transformCarData } from "@/lib/utils";
 export default async function Home() {
   const data = await getMonthlyData();
+  const formattedData = transformCarData(data);
   return (
-    <main>
-      <SheetsTable />
+    <main className="container mx-auto py-10">
+      <DataTable columns={columns} data={formattedData} />
     </main>
   );
 }
