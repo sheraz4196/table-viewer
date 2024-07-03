3e7e5ffcac9726ab94bbf2b3d051c5200281a31e
sheraz4196 <sheraztoor321@gmail.com>
2024-07-03 14:50:25 +0500

    fix:  sheduled the data fetching

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { columns } from "@/components/table/columns";
 import DataTable from "@/components/table/data-table";
 import { getMonthlyData } from "@/lib/new-data";
+import cron from "node-cron";
 import { transformCarData } from "@/lib/utils";
 export default async function Home() {
+  cron.schedule("* * * * *", getMonthlyData);
   const data = await getMonthlyData();
   const formattedData = transformCarData(data);
   return (
--- server/server.js
+++ server/server.js
@@ -8,23 +8,6 @@ const handle = app.getRequestHandler();
 app.prepare().then(async () => {
   const server = express();
   const httpServer = http.createServer(server);
-
-  // Scheduler
-  const runScheduler = async () => {
-    try {
-      const response = await axios.post(
-        `http://localhost:3000/api/services/scheduler`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   server.all("*", (req, res) => {
     return handle(req, res);
   });
