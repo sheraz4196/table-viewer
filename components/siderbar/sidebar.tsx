b557a64897f1ee4690ca96f72baff614b695e64a
sheraz4196 <sheraztoor321@gmail.com>
2024-07-12 17:59:46 +0500

    feat: functionality

--- components/siderbar/sidebar.tsx
+++ components/siderbar/sidebar.tsx
@@ -1,10 +1,18 @@
+"use client";
 import { ChevronDown, Lock, Plus, PlayCircle } from "lucide-react";
 import Image from "next/image";
 import { Button } from "../ui/button";
+import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 export default function Sidebar() {
+  const [openFavorite, setOpenFavorite] = useState(false);
+  const [allFavorites, setAllFavorites] = useState(false);
+  const [vehicles, setVehicles] = useState(false);
+  const [tryOpen, setTryOpen] = useState(false);
+  const [hotDeals, setHotDeals] = useState(false);
   return (
-    <div className="flex items-center justify-center w-56 relative">
+    <div className="flex items-center justify-center w-56 relative mt-40">
       <div className="flex flex-col gap-1.5">
         <div className="p-2">
           <Image src={"/logo.png"} alt="Logo" width={142} height={17} />
@@ -34,6 +42,7 @@ export default function Sidebar() {
           <button
             className="flex items-center gap-3 text-xs border-none !px-0"
             type="button"
+            onClick={() => setVehicles(!vehicles)}
           >
             Vehicles
             <Image
@@ -41,155 +50,203 @@ export default function Sidebar() {
               width={6}
               height={6}
               alt="Icon"
+              className={cn(vehicles ? "" : "-rotate-90")}
             />
           </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <Image src={"/icons/bmw.png"} width={16} height={16} alt="Icon" />
-            BMW
-          </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <Image src={"/icons/audi.png"} width={16} height={16} alt="Icon" />
-            Mercedes-Benz
-          </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <Image
-              src={"/icons/porsche.png"}
-              width={16}
-              height={16}
-              alt="Icon"
-            />
-            Porsche
-          </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <Image
-              src={"/icons/contact-gray.png"}
-              width={16}
-              height={16}
-              alt="Icon"
-            />
-            Audi
-          </button>
-        </div>
-        <div className="flex flex-col gap-3 p-2">
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            Favorites
-            <Image
-              src={"/icons/chev-down.png"}
-              width={6}
-              height={6}
-              alt="Icon"
-            />
-          </button>
-          <button
-            type="button"
-            className="flex items-center gap-3 text-xs border-none bg-[#efefef] px-4 py-2 rounded-sm"
-          >
-            <Image src={"/icons/list.png"} width={16} height={16} alt="Icon" />
-            HOT DEALS
-          </button>
-        </div>
-        <div className="flex flex-col gap-3 p-2">
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            Favorites
-            <Image
-              src={"/icons/chev-down.png"}
-              width={6}
-              height={6}
-              alt="Icon"
-            />
-          </button>
-          <button
-            type="button"
-            className="flex items-center gap-3 text-xs border-none !px-0"
-          >
-            <Image
-              src={"/icons/contact-indigo.png"}
-              width={16}
-              height={16}
-              alt="Icon"
-            />
-            HOT DEALS
-            <Lock className="size-4 text-zinc-600" />
-            <ChevronDown className="size-3" />
-          </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none ml-6 bg-[#efefef] px-4 py-2 rounded-sm"
-            type="button"
-          >
-            <Image
-              src={"/icons/backie.png"}
-              width={16}
-              height={16}
-              alt="Icon"
-            />
-            BMW
-          </button>
-          <ul className="flex flex-col border-l-2 border-gray-300">
-            <li>
+          {vehicles && (
+            <>
               <button
-                className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+                className="flex items-center gap-3 text-xs border-none !px-0"
                 type="button"
               >
-                Mercedes
+                <Image
+                  src={"/icons/bmw.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
+                BMW
               </button>
-            </li>
-            <li>
               <button
-                className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+                className="flex items-center gap-3 text-xs border-none !px-0"
                 type="button"
               >
+                <Image
+                  src={"/icons/audi.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
+                Mercedes-Benz
+              </button>
+              <button
+                className="flex items-center gap-3 text-xs border-none !px-0"
+                type="button"
+              >
+                <Image
+                  src={"/icons/porsche.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
                 Porsche
               </button>
-            </li>
-          </ul>
+              <button
+                className="flex items-center gap-3 text-xs border-none !px-0"
+                type="button"
+              >
+                <Image
+                  src={"/icons/contact-gray.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
+                Audi
+              </button>
+            </>
+          )}
+        </div>
+        <div className="flex flex-col gap-3 p-2">
           <button
-            className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+            className="flex items-center gap-3 text-xs border-none !px-0"
             type="button"
+            onClick={() => setOpenFavorite(!openFavorite)}
           >
-            <Image src={"/icons/audi.png"} width={16} height={16} alt="Icon" />
-            Audi
+            Favorites
+            <Image
+              src={"/icons/chev-down.png"}
+              width={6}
+              height={6}
+              alt="Icon"
+              className={cn(openFavorite ? "" : "-rotate-90")}
+            />
           </button>
+          {openFavorite && (
+            <button
+              type="button"
+              className="flex items-center gap-3 text-xs border-none bg-[#efefef] px-4 py-2 rounded-sm"
+            >
+              <Image
+                src={"/icons/list.png"}
+                width={16}
+                height={16}
+                alt="Icon"
+              />
+              HOT DEALS
+            </button>
+          )}
+        </div>
+        <div className="flex flex-col gap-3 p-2">
+          <button
+            className="flex items-center gap-3 text-xs border-none !px-0"
+            type="button"
+            onClick={() => setAllFavorites(!allFavorites)}
+          >
+            Favorites
+            <Image
+              src={"/icons/chev-down.png"}
+              width={6}
+              height={6}
+              alt="Icon"
+              className={cn(allFavorites ? "" : "-rotate-90")}
+            />
+          </button>
+          {allFavorites && (
+            <>
+              <button
+                type="button"
+                className="flex items-center gap-3 text-xs border-none !px-0"
+                onClick={() => setHotDeals(!hotDeals)}
+              >
+                <Image
+                  src={"/icons/contact-indigo.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
+                HOT DEALS
+                <Lock className="size-4 text-zinc-600" />
+                <ChevronDown
+                  className={cn(hotDeals ? "size-3" : "-rotate-90 size-3")}
+                />
+              </button>
+
+              {hotDeals && (
+                <button
+                  className="flex items-center gap-3 text-xs border-none ml-6 bg-[#efefef] px-4 py-2 rounded-sm"
+                  type="button"
+                >
+                  <Image
+                    src={"/icons/backie.png"}
+                    width={16}
+                    height={16}
+                    alt="Icon"
+                  />
+                  BMW
+                </button>
+              )}
+              <ul className="flex flex-col border-l-2 border-gray-300">
+                <li>
+                  <button
+                    className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+                    type="button"
+                  >
+                    Mercedes
+                  </button>
+                </li>
+                <li>
+                  <button
+                    className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+                    type="button"
+                  >
+                    Porsche
+                  </button>
+                </li>
+              </ul>
+              <button
+                className="flex items-center gap-3 text-xs border-none !px-0 ml-6"
+                type="button"
+              >
+                <Image
+                  src={"/icons/audi.png"}
+                  width={16}
+                  height={16}
+                  alt="Icon"
+                />
+                Audi
+              </button>
+            </>
+          )}
         </div>
         <div className="flex flex-col gap-3 p-2 mt-6">
           <button
             className="flex items-center gap-3 text-xs border-none !px-0"
             type="button"
+            onClick={() => setTryOpen(!tryOpen)}
           >
             Try
-            <ChevronDown className="size-3" />
+            <ChevronDown
+              className={cn("size-3", cn(tryOpen ? "" : "-rotate-90"))}
+            />
           </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <Plus className="size-4" />
-            Share This Website
-          </button>
-          <button
-            className="flex items-center gap-3 text-xs border-none !px-0"
-            type="button"
-          >
-            <PlayCircle className="size-4" />
-            Updated Today
-          </button>
+          {tryOpen && (
+            <>
+              <button
+                className="flex items-center gap-3 text-xs border-none !px-0"
+                type="button"
+              >
+                <Plus className="size-4" />
+                Share This Website
+              </button>
+              <button
+                className="flex items-center gap-3 text-xs border-none !px-0"
+                type="button"
+              >
+                <PlayCircle className="size-4" />
+                Updated Today
+              </button>
+            </>
+          )}
         </div>
       </div>
       <Button
