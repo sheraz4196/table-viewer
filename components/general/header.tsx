7d90d52124da4c8e260d776e12a7fc66d7cac880
sheraz4196 <sheraztoor321@gmail.com>
2024-07-15 11:01:53 +0500

    fix: button

--- components/general/header.tsx
+++ components/general/header.tsx
@@ -9,21 +9,15 @@ import {
 } from "@/components/ui/drawer";
 import Sidebar from "../siderbar/sidebar";
 import { useTheme } from "next-themes";
-import { SlidersHorizontal } from "lucide-react";
+import { PanelRight, SlidersHorizontal } from "lucide-react";
 export default function Header() {
   const { theme } = useTheme();
   return (
     <header className="p-6 flex items-center justify-between border-b">
       <div className="px-4 flex items-center gap-3">
         <Drawer direction="left">
-          <DrawerTrigger className="lg:hidden">
-            <Image
-              src={"/sidebar.png"}
-              alt="sidebar"
-              width={18}
-              height={18}
-              className=" brightness-0 dark:brightness-100"
-            />
+          <DrawerTrigger className="lg:hidden self-start">
+            <PanelRight className="size-3" />
           </DrawerTrigger>
 
           <DrawerContent className="!w-1/2 h-screen rounded-none fixed overflow-hidden overflow-y-auto flex items-center">
